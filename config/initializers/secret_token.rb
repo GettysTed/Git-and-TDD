4d0c791c77c0c8b2a9f2d1ae1ee2c36b1a3a280d
Ted Kerr <kerrth02@gettysburg.edu>
2012-10-06 13:02:39 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GitAndTdd::Application.config.secret_token = '85e2023b0b0a8c79bafdb44d89a14963ebd48c63accfc63bfdf117e77b775f8a233b0024307c1d73d8de562920e602ddf6bb6a0439014e995783203900fce9b2'
